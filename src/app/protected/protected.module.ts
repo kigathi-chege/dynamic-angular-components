39ed33571b465dc9da4d54baa0b317f3740ccce2
kigathi-chege <chegekigathi@gmail.com>
2023-05-11 18:28:59 +0300

    create dynamic components

--- src/app/protected/protected.module.ts
+++ src/app/protected/protected.module.ts
@@ -5,17 +5,21 @@ import { ProtectedRoutingModule } from './protected-routing.module';
 import { ProtectedComponent } from './protected.component';
 import { DepartmentComponent } from './department/department.component';
 import { DynamicDirective } from './department/dynamic.directive';
-
+import { ClawesomeComponent } from './department/clawesome.component';
+import { PawesomeComponent } from './department/pawesome.component';
+import { SmileyComponent } from './department/smiley.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     ProtectedComponent,
     DepartmentComponent,
-    DynamicDirective
+    DynamicDirective,
+    ClawesomeComponent,
+    PawesomeComponent,
+    SmileyComponent,
   ],
-  imports: [
-    CommonModule,
-    ProtectedRoutingModule
-  ]
+  imports: [CommonModule, ProtectedRoutingModule, MatCardModule, MatListModule],
 })
-export class ProtectedModule { }
+export class ProtectedModule {}
